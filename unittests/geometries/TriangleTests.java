78f589b35940e9aa350cf5fe08872c963513715a
eliel <elielmonf@gmail.com>
2024-01-12 13:53:58 +0200

    done

--- unittests/geometries/TriangleTests.java
+++ unittests/geometries/TriangleTests.java
@@ -42,7 +42,7 @@ class TriangleTests {
      */
     @Test
     void testfindIntsersections() {
-
+        
     }
     //###########################################################################
 }
