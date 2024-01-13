e5643ca969cb7d9253b1ed4771af7e10da29bc2a
Ariel-Atias <arielatias66@gmail.com>
2024-01-13 22:22:34 +0200

    done

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -1,5 +1,21 @@
 package geometries;
 
 public class Geometries {
+    private list<Intersectable> geometriesList = new LinkedList<>();
+    public Geometries() {
+    }
+    public Geometries(Intersectable... geometries) {
+        this(); // קריאה לבנאי ברירת המחדל
+        add(geometries);
+    }
 
+    // מתודה להוספת גופים לרשימה
+    public void add(Intersectable... geometries) {
+        for (Intersectable geometry : geometries) {
+            geometriesList.add(geometry);
+        }
+    }
+    public List<Intersection> findIntersections(Ray ray) {
+        return null;
+    }
 }
--- unittests/geometries/GeometriesTests.java
+++ unittests/geometries/GeometriesTests.java
@@ -1,5 +1,10 @@
 package geometries;
 
-public class GeometriesTests {
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+public class GeometriesTests {
+    public void testFindIntersections() {
+        assertNull( geometriesList , "The bodies collection is empty");
+
+    }
 }
