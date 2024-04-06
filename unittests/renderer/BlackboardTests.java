cf13484130d0b60b818351fc37d0c708b3c48a69
eliel <elielmonf@gmail.com>
2024-04-07 00:33:49 +0300

    done

--- unittests/renderer/BlackboardTests.java
+++ unittests/renderer/BlackboardTests.java
@@ -73,7 +73,6 @@ public class BlackboardTests {
         // High Anti-Aliasing for 9X9 for testing
         camera.setImageWriter(new ImageWriter("AntiAliasingTestImage9X9", 1000,1000))
                 .setAntiAliasing(9, 9)
-                .setRayTracer(new SimpleRayTracer(scene))
                 .build()
                 .renderImage()
                 .writeToImage();
@@ -82,20 +81,19 @@ public class BlackboardTests {
     @Test
     void softShadowsTest() {
 
+        // A Soft-Shadows with a blackboard divided into 2x2 (the Soft-Shadows effect is weak)
         final Scene scene1 = new Scene("Test Soft-Shadows");
-        final Scene scene2 = new Scene("Test Soft-Shadows");
         scene1.geometries.add(
                 new Plane(Point.ZERO, new Point(0, 0, 1), new Point(0, 1, 0))
                         .setEmission(new Color(129, 133, 137))
                         .setMaterial(new Material().setKd(0.5)),
 
-                new Sphere(5.001d, new Point(0, 0, 5)).setEmission(new Color(137, 148, 153))
+                new Sphere(5d, new Point(0, 0, 5)).setEmission(new Color(137, 148, 153))
                         .setMaterial(new Material().setKs(0.4).setShininess(70))
         );
         scene1.lights.add(
-                new SpotLight(new Color(255,230,89), new Point(15, 0, -10), new Vector(-1, 0, 1), 1, 1, 2, 2).setKl(0.1).setKq(0.002)
+                new SpotLight(new Color(255,230,89), new Point(15, 0, -10), new Vector(-1, 0, 1), 2, 2, 2, 2).setKl(0.1).setKq(0.002)
         );
-
         camera
                 .setImageWriter(new ImageWriter("SoftShadowsTestImage2X2", 1000,1000))
                 .setRayTracer(new SimpleRayTracer(scene1))
@@ -103,11 +101,69 @@ public class BlackboardTests {
                 .renderImage()
                 .writeToImage();
 
-        // TODO: Adding soft shadow at 9X9
+
+        //A Soft-Shadows with a blackboard divided into 9x9 (the Soft-Shadows effect is strong)
+        final Scene scene2 = new Scene("Test Soft-Shadows");
+        scene2.geometries.add(
+                new Plane(Point.ZERO, new Point(0, 0, 1), new Point(0, 1, 0))
+                        .setEmission(new Color(129, 133, 137))
+                        .setMaterial(new Material().setKd(0.5)),
+
+                new Sphere(5d, new Point(0, 0, 5)).setEmission(new Color(137, 148, 153))
+                        .setMaterial(new Material().setKs(0.4).setShininess(70))
+        );
+        scene2.lights.add(
+                new SpotLight(new Color(255,230,89), new Point(15, 0, -10), new Vector(-1, 0, 1), 2, 2, 9, 9).setKl(0.1).setKq(0.002)
+        );
+        camera
+                .setImageWriter(new ImageWriter("SoftShadowsTestImage9X9", 1000,1000))
+                .setRayTracer(new SimpleRayTracer(scene2))
+                .build()
+                .renderImage()
+                .writeToImage();
     }
 
     @Test
     void AntiAliasing_And_SoftShadowTest() {
-        // TODO: TEST
+        final Scene scene = new Scene("Test Anti-Aliasing and Soft-Shadows");
+        scene.setBackground(new Color(135, 206, 235));
+        scene.geometries.add(
+                new Plane(Point.ZERO, new Point(1, 0, 0), new Point(0, 1, 0))
+                        .setEmission(new Color(55, 127, 3))
+                        .setMaterial(new Material().setKd(0.5)),
+
+                new Sphere(2d, new Point(0, -4, 2.1)).setEmission(new Color(255, 0, 0))
+                        .setMaterial(new Material().setKs(0.4).setShininess(70)),
+
+                new Triangle(new Point(6, -10, 0), new Point(-6, -10, 0), new Point(6, -5, 10))
+                        .setEmission(new Color(0, 0, 255)),
+
+                new Triangle(new Point(-6, -5, 10), new Point(-6, -10, 0), new Point(6, -5, 10))
+                        .setEmission(new Color(0, 0, 255)),
+
+                new Triangle(new Point(6, -10, 0), new Point(8, -5, 0), new Point(6, -5, 10))
+                        .setEmission(new Color(0, 0, 255)),
+
+                new Triangle(new Point(-6, -10, 0), new Point(-6, -5, 10), new Point(-8, -5, 0))
+                        .setEmission(new Color(0, 0, 255))
+        );
+        scene.lights.add(
+                new SpotLight(new Color(255,230,89), new Point(4, -5, 6), new Vector(-1, 0, -1)).setKl(0.1).setKq(0.002)
+        );
+
+        scene.lights.add(
+                new PointLight(new Color(255,230,89), new Point(-4, -5, 6)).setKl(0.1).setKq(0.002)
+        );
+
+        scene.lights.add(new DirectionalLight(new Color(80, 50, 0), new Vector(0, 1, -1))
+        );
+        camera
+                .setLocation(new Point(0, 55, 60))
+                .setDirection(new Vector(0, -1, -1), new Vector(0, -1, 1))
+                .setImageWriter(new ImageWriter("AntiAliasing+SoftShadowsTestImage", 1000,1000))
+                .setRayTracer(new SimpleRayTracer(scene))
+                .build()
+                .renderImage()
+                .writeToImage();
     }
 }
